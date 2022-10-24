d74c701584d11a0e350558e5cfc886300f801b5e
Michal Winiecki <falrond@gmail.com>
2022-10-24 13:58:13 +0200

    Acheiving CORS

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const {
 } = require("./controllers/productController");
 
 const server = http.createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
   if (req.url === "/api/products" && req.method === "GET") {
     getProducts(req, res);
   } else if (
